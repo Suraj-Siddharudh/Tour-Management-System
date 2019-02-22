12ad9899a4c2fb2b618afa8fc357c59e71ec4cfc
Suraj <ssuraj.here@gmail.com>
2019-02-22 00:03:11 -0500

    Google Auth Fixes

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -8,7 +8,7 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
         set_flash_message(:notice, :success, kind: "google") if is_navigational_format?
       else
         session["devise.google_data"] = request.env["omniauth.auth"]
-        redirect_to new_user_registration_url
+        redirect_to new_user_registration
       end
     end
   
