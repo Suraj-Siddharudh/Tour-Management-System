0ad10ec51987ebb20c6804aa30baebdab2376a07
Suraj <ssuraj.here@gmail.com>
2019-02-22 01:38:52 -0500

    Permission Issue with User and Devise

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -83,5 +83,6 @@ end
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
       params.fetch(:user, {})
+      params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks" }
+  devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks", registrations: "users/registrations"}
   resources :tours
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     # Custom Routes for Users
