f6d30d66ad4aa51b6a2be86330569ca75dc3c7c3
aish6595 <aish6595@gmail.com>
2019-02-23 21:35:27 -0500

    Validation

--- app/models/tour.rb
+++ app/models/tour.rb
@@ -1,3 +1,7 @@
 class Tour < ApplicationRecord
 	belongs_to :user, optional: true 
+	validates_presence_of :Name, :pickup, :status, :countries, :states, :Description
+	validates_numericality_of :total_seats, :avail_seats, greater_than: 0, less_than_or_equal_to: 100, presence: true
+	validates_numericality_of :avail_waitlist, :Price, greater_than: 0, presence: true
+	
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ApplicationRecord
          :omniauthable, :omniauth_providers => [:google]
 
  validates_presence_of :email, :first_name
+#  validates :password, length: { in: 6..20 }, confirmation: true
+
  has_many :tour
 
  def self.from_omniauth(auth)
