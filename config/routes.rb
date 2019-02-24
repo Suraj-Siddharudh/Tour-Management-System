622e04e25264be8e8c4c371e90e3b1c1f9185f4d
Suraj <ssuraj.here@gmail.com>
2019-02-23 21:15:25 -0500

    User Profile Functionality and Bootstrap additions for Forms

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
     # Custom Routes for Users
     match '/user/new', to: 'users#new', via: 'get'
     match '/user/create', to: 'users#create', via: 'post'
-    match '/user/:id/edit', to: 'users#edit', via: 'get', as: 'user_create_path'
+    match '/user/:id/edit', to: 'users#edit', via: 'get', as: 'user_edit'
     match '/user/:id', to: 'users#update', via: 'put'
     match '/user/:id', to: 'users#update', via: 'patch'
     match '/user/:id', to: 'users#destroy', via: 'delete'
