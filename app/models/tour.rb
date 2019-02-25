9224c48aa76bec788bea99bbd3ca8f8db2e76020
Suraj <ssuraj.here@gmail.com>
2019-02-25 00:34:29 -0500

    Updated Seed, Bookmark RBAC, Updated UI

--- app/models/tour.rb
+++ app/models/tour.rb
@@ -2,8 +2,8 @@ class Tour < ApplicationRecord
 	belongs_to :user, optional: true 
 	
 	validates_presence_of :Name, :pickup, :status, :countries, :states, :Description
-	validates_numericality_of :total_seats, :avail_seats, greater_than: 0, less_than_or_equal_to: 100, presence: true
-	validates_numericality_of :avail_waitlist, :Price, greater_than: 0, presence: true
-	
+	validates_numericality_of :total_seats, greater_than: 0, less_than_or_equal_to: 100, presence: true #avail_seats
+	# validates_numericality_of :avail_waitlist, :Price, greater_than: 0, presence: true
+	#Removing validation on avail seats and waitlist
 	has_many :bookings
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -17,6 +17,16 @@ User.create!([{
   is_admin: 1,
   phone_number: '1800-12-1234',
 }])
+User.create!([{
+  first_name: 'Customer', 
+  last_name: 'K', 
+  email: 'customer@touringapp.com',
+  password: 'customer123', 
+  is_customer: 1, 
+  is_agent: 0, 
+  is_admin: 0,
+  phone_number: '1800-12-1236',
+}])
 
 User.create!([{
   first_name: 'Agent', 
@@ -29,28 +39,63 @@ User.create!([{
   phone_number: '1800-12-1235',
 }])
 
-User.create!([{
-  first_name: 'Customer', 
-  last_name: 'K', 
-  email: 'customer@touringapp.com',
-  password: 'customer123', 
-  is_customer: 1, 
-  is_agent: 0, 
-  is_admin: 0,
-  phone_number: '1800-12-1236',
-}])
-
-
-
-20.times do
+# Create 15 Agents
+15.times do
   User.create(
     first_name: Faker::Name.unique.first_name, 
     last_name: Faker::Name.last_name, 
     email: Faker::Internet.email,
     password: 'password', 
-    is_customer: Faker::Boolean.boolean, 
-    is_agent: Faker::Boolean.boolean, 
+    is_customer: 0, 
+    is_agent: 1, 
     is_admin: 0,
     phone_number: Faker::PhoneNumber.phone_number
+  )
+end
+# Create 10 Customers
+10.times do
+  User.create(
+    first_name: Faker::Name.unique.first_name, 
+    last_name: Faker::Name.last_name, 
+    email: Faker::Internet.email,
+    password: 'password', 
+    is_customer: 1, 
+    is_agent: 0, 
+    is_admin: 0,
+    phone_number: Faker::PhoneNumber.phone_number
+  )
+end
+
+5.times do
+  Tour.create(
+    Name: Faker::Company.name, 
+    Description: Faker::Marketing.buzzwords, 
+    Price: Faker::Number.between(0, 3),
+    start_date: Faker::Date.forward(30), 
+    end_date: Faker::Date.between(1.month.from_now,2.month.from_now), 
+    pickup: Faker::Boolean.boolean, 
+    total_seats: Faker::Number.between(10, 90),
+    status: "Active",
+    booking_deadline: Faker::Date.forward(30),
+    countries: Faker::Address.country,
+    states: Faker::Address.state,
+    user_id: 3
+  )
+end
+
+10.times do
+  Tour.create(
+    Name: Faker::Company.name, 
+    Description: Faker::Marketing.buzzwords, 
+    Price: Faker::Number.between(0, 3),
+    start_date: Faker::Date.forward(30), 
+    end_date: Faker::Date.between(1.month.from_now,2.month.from_now), 
+    pickup: Faker::Boolean.boolean, 
+    total_seats: Faker::Number.between(10, 90),
+    status: "Active",
+    booking_deadline: Faker::Date.forward(30),
+    countries: Faker::Address.country,
+    states: Faker::Address.state,
+    user_id: Faker::Number.between(3, 18)
   )
 end
