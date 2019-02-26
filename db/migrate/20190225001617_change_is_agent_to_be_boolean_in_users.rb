e0cd4ae5bfaa3a241f8fabd10f45149d14ce89b8
Suraj <ssuraj.here@gmail.com>
2019-02-25 19:03:06 -0500

    Heroku deployment Migration issues

--- db/migrate/20190225001617_change_is_agent_to_be_boolean_in_users.rb
+++
@@ -1,6 +0,0 @@
-class ChangeIsAgentToBeBooleanInUsers < ActiveRecord::Migration[5.2]
-  def change
-    change_column :users, :is_agent, :boolean
-    change_column :users, :is_customer, :boolean
-  end
-end
