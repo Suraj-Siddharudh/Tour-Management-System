0420174ceb8ced46f35dc5b61cec3018f538466d
Suraj <ssuraj.here@gmail.com>
2019-03-01 14:26:09 -0500

    Backend validation to not delete Admin account

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -94,11 +94,13 @@ class UsersController < ApplicationController
   # DELETE /users/1
   # DELETE /users/1.json
   def destroy
+    if params[:id]!= 1
     @user.destroy
     respond_to do |format|
       format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
       format.json { head :no_content }
     end
+  end
   end
 
   def update
