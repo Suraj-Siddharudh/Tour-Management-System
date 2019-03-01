601837dff189d8a21b75bdb20fae78d67b500d47
Suraj <ssuraj.here@gmail.com>
2019-03-01 12:40:43 -0500

    Validations, Tour Booking UI and RBAC

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,6 @@
 class Booking < ApplicationRecord
 	belongs_to :user
 	belongs_to :tour
+
+	validates :no_of_seats, presence: true
 end
--- app/models/tour.rb
+++ app/models/tour.rb
@@ -2,13 +2,25 @@ class Tour < ApplicationRecord
 	belongs_to :user, optional: true 
 	before_create :set_available_seats
 	
+	has_many :bookings
+	has_many :bookmark
+	mount_uploader :image, ImageUploader
+	serialize :image, JSON
+	
+
 	validates_presence_of :Name, :pickup, :status, :countries, :states, :Description
 	validates_numericality_of :total_seats, greater_than: 0, less_than_or_equal_to: 100, presence: true #avail_seats
 	validates_numericality_of  :Price, greater_than: 0, less_than_or_equal_to: 300000
-	#Removing validation on avail seats and waitlist
-	has_many :bookings
-	mount_uploader :image, ImageUploader
-	serialize :image, JSON
+	validates :start_date, presence: true
+	validates :end_date, presence: true
+	validates :booking_deadline, presence: true
+	validate :valid_end_date?
+
+
+  def valid_end_date?
+	  errors.add(:start_date, "must be before end_date") unless start_date < end_date
+end
+
 
 	private
 
