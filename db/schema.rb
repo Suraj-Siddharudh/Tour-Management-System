3a2781ff66bf2d46b553e1a1cb274386b448775e
Suraj <ssuraj.here@gmail.com>
2019-03-01 19:58:41 -0500

    Bug fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_01_200241) do
 ActiveRecord::Schema.define(version: 2019_03_01_225531) do
 
   create_table "bookings", force: :cascade do |t|
